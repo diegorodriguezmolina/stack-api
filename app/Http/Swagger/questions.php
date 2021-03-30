0dbc2a7eebcea8f05366026367af0c78c43f9814
drodriguez <drodriguez@omatech.com>
2021-03-30 10:04:24 +0200

    Update docu

---
+++ app/Http/Swagger/questions.php
@@ -0,0 +1,33 @@
+<?php
+/** @OA\Info(title="Stack API", version="0.1") */
+/** @OA\Tag(name="questions") **/
+
+/**
+ * @OA\Get(
+ *      path="/api/questions",
+ *      summary="List of questions that tagged",
+ *      description="List of questions that tagged",
+ *      tags={"questions"},
+ *      @OA\Parameter(
+ *          required=true,
+ *          name="tagged",
+ *          in="query"
+ *      ),
+ *      @OA\Parameter(
+ *          required=false,
+ *          name="fromDate",
+ *          in="query"
+ *      ),
+ *      @OA\Parameter(
+ *          required=false,
+ *          name="toDate",
+ *          in="query"
+ *      ),
+ *      @OA\Response(response=200, description="",
+ *         @OA\MediaType(
+ *            mediaType="application/json"
+ *         )
+ *      ),
+ *      security={{"bearerAuth":{}}}
+ *   )
+ */
